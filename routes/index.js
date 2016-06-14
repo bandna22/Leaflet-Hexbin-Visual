ed98d9c6857c42e43ed24388927eea7e93eb2391
bandna22 <kalra.bandna@gmail.com>
2016-06-14 14:22:16 +0530

    Airquality code
    Commit on master

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,41 @@
 
-// routes/index.js
+
 var express = require('express');
 var app = express();
 var fs = require("fs");
         console.log("\n *START* \n");
-		 var content = JSON.parse(fs.readFileSync('./values.json', 'utf8'));
-        //var content = fs.readFileSync("./values.json");
+         var content = JSON.parse(fs.readFileSync('./airquality.json', 'utf8'));
         console.log("Output Content : \n"+ content);
-		console.log("Output Array : \n"+ JSON.stringify(content));
-		console.log("Output object : \n"+ content[0].lat);
-		console.log("Output length : \n"+ content.length);
+        console.log("Output Array : \n"+ JSON.stringify(content));
+        console.log("Output object : \n"+ content.places);
+        var placearray = content.places;
+        console.log("Output length : \n"+ placearray.length);
         console.log("\n *EXIT* \n");
-		var latarray=[];
-		var longarray=[];
-		var moistarray=[];
-		for (i=0;i<content.length;i++)
-		{
-			latarray[i]=content[i].lat;
-			
-		}
-		for (i=0;i<content.length;i++)
-		{
-			longarray[i]=content[i].long;
-			
-		}
-		for (i=0;i<content.length;i++)
-		{
-			moistarray[i]=content[i].moisture[0]*100;
-			
-		}		
-		
+        var latarray=[];
+        var longarray=[];
+        var aqarray=[];
+        for (i=0;i<placearray.length;i++)
+        {
+            latarray[i]=placearray[i].latitude_place1;
+            
+        }
+        for (i=0;i<placearray.length;i++)
+        {
+            longarray[i]=placearray[i].longitude_place1;
+            
+        }
+        for (i=0;i<placearray.length;i++)
+        {
+            console.log(placearray[i].air_quality_place1);
+            aqarray[i]=placearray[i].air_quality_place1;
+            
+        }
+        for (i=0;i<placearray.length;i++)
+        {
+            placearray[i]=placearray[i].Place;
+            
+        }       
+        
 app.set('view engine', 'pug');
  
 /* GET home page. */
@@ -42,9 +48,10 @@ app.get('/map', function(req,res) {
         res.render('map', {
        
             values:latarray,
-			longvalues:longarray,
-			moistvalues:moistarray
-			
+            longvalues:longarray,
+            aqvalues:aqarray,
+            placesvalues:placearray
+            
         });
    
 }); 
